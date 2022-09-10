4ebf113d915a3baaa609c24548717b017580f930
Cleide Gomides <cleidegomides@hotmail.com>
2022-09-10 18:09:44 -0300

    DataBase

---
+++ backend/src/main/java/com/devsuperior/backend/entities/Sale.java
@@ -0,0 +1,22 @@
+package com.devsuperior.backend.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "tb_sales")
+ public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+}
