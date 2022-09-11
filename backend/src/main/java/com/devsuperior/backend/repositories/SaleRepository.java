b5d69a3faa03646ab7a1e36581faa66bb34dd676
Cleide Gomides <cleidegomides@hotmail.com>
2022-09-11 19:18:13 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/backend/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.backend.repositories;
+
+import com.devsuperior.backend.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
---
+++ backend/src/main/java/com/devsuperior/backend/services/SaleService.java
@@ -0,0 +1,19 @@
+package com.devsuperior.backend.services;
+
+import com.devsuperior.backend.entities.Sale;
+import com.devsuperior.backend.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    public List<Sale>  findSale() {
+        return repository.findAll();
+    }
+}
